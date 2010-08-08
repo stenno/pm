53428638d54ebf7271b14f1a5893c588614f2e65
stenno <stenno@host.(none)>
2010-08-08 18:54:19 +0200

    gitignore updated

--- evently/menu/loggedIn/selectors/a[href=#desktop].js
+++ evently/menu/loggedIn/selectors/a[href=#desktop].js
@@ -1,3 +1,3 @@
 function() {
-	$$(this).trigger("loggedIn",[]);
+	$(this).trigger("desktop", []);
 }
--- evently/projects/loggedIn/query.js
+++
@@ -1,8 +0,0 @@
-function() {
-	var user = $$("#account").userCtx.name;
-	return {
-		view:"user-projects",
-		group:true,
-		key: unescape(user) 
-	}
-}
